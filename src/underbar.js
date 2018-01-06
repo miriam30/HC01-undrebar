91003cadfde4eac4b1cf8f3a10edae12d42ddee9
miriam30 <einstein@miriam.alvarez.p96@gmail.com>
2018-01-05 20:52:04 -0600

    finished part 1

--- src/underbar.js
+++ src/underbar.js
@@ -123,9 +123,9 @@
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
    var results = []
-    _.each(collection, function(item)){
-
-    }
+    _.each(collection, function(item){
+        results.push(iterator(item))
+    })
 
     return results
   };
